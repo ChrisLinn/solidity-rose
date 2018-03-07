0218b572a177148930bce118792cd4ae686f0259
ChrisLinn <nanyishanshaoxiu@gmail.com>
2018-03-07 22:10:25 +0800

    inheritance done

---
+++ RoseBreeding.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.18;
+
+import "./RoseOwnership.sol";
+
+contract RoseBreeding is RoseOwnership {}
---
+++ RoseOwnership.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.18;
+
+import "./RoseBase.sol";
+
+contract RoseOwnership is RoseBase, ERC721 {}
